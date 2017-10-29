1d03e0b84819ecb7338199ac969db1f8a39225a0
niklus <niklasoti@gmail.com>
2017-10-29 21:17:31 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -5,19 +5,14 @@ import state from './state'
 import view from './views'
 import modules from "./modules"
 //import { router } from "@hyperapp/router"
-import logger from "@hyperapp/logger"
-import logCRP from './utils/measure_crp'
+//import logger from "@hyperapp/logger"
+//import logCRP from './utils/measure_crp'
 
 /* APP */
-logger()(app)({ 
+app({ 
 	actions, 
 	state, 
 	view,
 	modules,
 	init(s, { init }){ init() }
 }, document.getElementById('app'))
-
-
-/* CRP */
-logCRP();
-
